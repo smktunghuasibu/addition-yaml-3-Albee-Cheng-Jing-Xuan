5da42c8a589162cbc6fa49a6295a392657d70e74
Albee-Cheng-Jing-Xuan <163367736+Albee-Cheng-Jing-Xuan@users.noreply.github.com>
2024-08-25 11:48:17 +0800

    Update add.py 2
    complete the given code by getting the marks

--- add.py
+++ add.py
@@ -4,9 +4,9 @@ def sum(x, y):
 
 def addition():
     a = int(input('Enter 1st number: '))
-    b = int(......('Enter 2nd number: '))
-    total = sum(......)
-    print(f'Sum of {a} and {b} is {......}')
+    b = int(input('Enter 2nd number: '))
+    total = sum(a, b)
+    print(f'Sum of {a} and {b} is {total}')
 
 ## JANGAN UBAH KOD DI BAWAH ##
 if __name__ == "__main__":
